a06bf3f145237776e12986b20384cfe27db294a9
smashanto <antoine.verseau.83@gmail.com>
2025-12-18 10:53:37 +0100

    Add MySQL support and implement UserServiceImpl
    Added MySQL JDBC dependency to pom.xml and created UserServiceImpl to fetch users from the MySQL database. This enables database-backed user retrieval in the application.

---
+++ api-users/src/main/java/fr/isen/api/UserServiceImpl.java
@@ -0,0 +1,35 @@
+package fr.isen.api;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserServiceImpl extends UserService { //implements UserService {
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/isen";
+    private static final String JDBC_USER = "root";
+    private static final String JDBC_PASSWORD = null;
+    // @Override
+    public List<User> GetUsers() {
+
+        List<User> users = new ArrayList<>();
+        try (Connection conn = DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD);
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery("SELECT * FROM user")) {
+            while (rs.next()) {
+                User user = new User();
+                user.setId(rs.getInt("id"));
+                user.setEmail(rs.getString("email"));
+                user.setName(rs.getString("name"));
+
+                users.add(user);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return users;
+    }
+}
